d9129256809dc6b36b48f9b74dc95595991eb9e3
SaiPrakash Kokkiligadda <SaiPrakash.Kokkiligadda@coxautoinc.com>
2023-11-13 09:17:51 -0500

    for loop included

--- nat.py
+++ nat.py
@@ -7,31 +7,33 @@ profiles= []
 with open(f"./config.yaml") as ymlfile:
     data = yaml.safe_load(ymlfile)
 
-session = boto3.Session(region_name=data['credentials']['region'],profile_name=profiles)
-client = session.client('ec2') 
-def get_nats():
-    response = client.describe_nat_gateways(MaxResults=100)
-    data = response.get("NatGateways")
-    while 'NextToken' in response and response.get('NextToken'):
-            token = response.get('NextToken')
-            response = client.describe_nat_gateways(MaxResults=100,NextToken=token)
-            data += response.get("NatGateways")
-    return data
+for profile in profiles:
+        session = boto3.Session(region_name=data['credentials']['region'],profile_name=profile)
+        client = session.client('ec2') 
+        def get_nats():
+            response = client.describe_nat_gateways(MaxResults=100)
+            data = response.get("NatGateways")
+            while 'NextToken' in response and response.get('NextToken'):
+                    token = response.get('NextToken')
+                    response = client.describe_nat_gateways(MaxResults=100,NextToken=token)
+                    data += response.get("NatGateways")
+            return data
 
-lbs =get_nats()
+        lbs =get_nats()
 
-time_now = datetime.now()
-with open(f"report.csv",mode='w') as csvfile:
-    fieldnames = ["PublicIp","VpcId","SubnetId",""]
-    csv_writer = csv.DictWriter(csvfile,fieldnames=fieldnames) 
-    csv_writer.writeheader()
-    for nat in get_nats:
-        public_ip = nat["NatGatewayAddresses"]["PublicIp"]
-        vpc_id    = nat["VpcId"]
-        subnet_id = nat["SubnetId"]
-        
-        csv_writer.writerow({
-                    "PublicIp":public_ip,
-                    "VpcId":vpc_id,
-                    "SubnetId": subnet_id})
-        
+        time_now = datetime.now()
+        with open(f"report.csv",mode='w') as csvfile:
+            fieldnames = ["profile","PublicIp","VpcId","SubnetId",""]
+            csv_writer = csv.DictWriter(csvfile,fieldnames=fieldnames) 
+            csv_writer.writeheader()
+            for nat in get_nats:
+                public_ip = nat["NatGatewayAddresses"]["PublicIp"]
+                vpc_id    = nat["VpcId"]
+                subnet_id = nat["SubnetId"]
+                
+                csv_writer.writerow({
+                            "Account" : profile,
+                            "PublicIp":public_ip,
+                            "VpcId":vpc_id,
+                            "SubnetId": subnet_id})
+                
