4d1e5ac6292919f32fb08c550176897807ef330f
whitewalker <42011633+RajithSangireddy@users.noreply.github.com>
2023-11-14 16:56:15 -0500

    Update iterate-all-accounts.sh

--- iterate-all-accounts.sh
+++ iterate-all-accounts.sh
@@ -1,1 +1,7 @@
- for p in $(aws configure list-profiles | grep baseline); do echo $p && export AWS_PROFILE=$p && python3 wiz_script_v2.py --data; done
+https://github.com/benkehoe/aws-sso-util
+export AWS_DEFAULT_SSO_START_URL= <value>
+export AWS_DEFAULT_SSO_REGION= <value>
+aws-sso-util configure populate --region <region-name>
+
+## run the below script to login to each account
+for p in $(aws configure list-profiles | grep <keyword>); do echo $p && export AWS_PROFILE=$p && <script.sh/script.py>; done
